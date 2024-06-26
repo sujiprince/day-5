a87f99f87114b2295743662e7834a0ac444a08dc
sujith <sujikn73@gmail.com>
2024-06-26 23:39:40 +0400

    resume using JSON

---
+++ resume.js
@@ -0,0 +1,75 @@
+let myResume={
+    "basics": {
+      "name": "Sujith Kannan",
+      "email": "sujilkn73@gamil.com",
+      "phone": 8148263189,
+      "degree": "Bsc (computer science)",
+      "location": {
+        "address": "9/81 ketti",
+        "postalCode": 643215,
+        "city": "The niligiris",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          
+          "github":"https://github.com/sujiprince"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Aircel production office",
+        "position": "Business Executive",
+        "experience": "6 months(2015-mar to 2015-oct)",
+        "location": "coimbatore",
+
+        "company": "The knit gallery",
+        "position": "assistant accoutant",
+        "experience": "6 months(2015-nov to 2016-may)",
+        "location": "coimbatore",
+        
+        "company": "Perfect associates",
+        "position": "Supervisor",
+        "experience": "1 year(2016-may to 2017-jun)",
+        "location": "coimbatore",
+        
+        "company": "Cavpain co ltd",
+        "position": "production, sales, account department",
+        "experience": "7 years(2017-jul to 2024-jul)",
+        "location": "Mauritius",
+        
+      },
+    ],
+    "education": [
+      {
+        "institution": "Maharaja arts and science college",
+        "department": "Computer science",
+        "studyType": "fulltime",
+        "batch start year": 2012,
+        "batch end year": 2015,
+        
+      },
+    ],
+    "skills": [
+      {
+        "name": "c++,java, javascript",
+        "level": "beginer",
+        "project": [
+          "Remote virtual network"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish,malayalam, frence(basic)",
+      }
+    ],
+    "interests": [
+      {
+        "name": "cricket, online games, watching movies in theatres",
+      }
+    ]
+  }
+  console.log(myResume);
